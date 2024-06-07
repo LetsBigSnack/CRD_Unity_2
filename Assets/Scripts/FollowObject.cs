20fd7dfb7799e905ed84faf3c56be6705f50bc04
LetsBigSnack <83255386+LetsBigSnack@users.noreply.github.com>
2024-06-07 15:28:27 +0200

    Initial Project State | Before Videos

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToFollow;
+    // Start is called before the first frame update
+
+    private Transform otherObjectTransform;
+    private Transform ownTransform;
+    private Vector3 offsetOfFollowObjectAndOwnObject;
+    void Start()
+    {
+        otherObjectTransform = objectToFollow.GetComponent<Transform>();
+        ownTransform = gameObject.GetComponent<Transform>();
+        offsetOfFollowObjectAndOwnObject = ownTransform.position - otherObjectTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = otherObjectTransform.position + offsetOfFollowObjectAndOwnObject;
+        otherObjectTransform.forward = new Vector3(1f, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+internal enum MovementType
+{
+    TransformBased,
+    PhysicsBased
+}
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _velocity = 1;
+    [SerializeField]
+    private float _jumpForce = 2;
+    [SerializeField]
+    private ForceMode selectedForceMode;
+    [SerializeField]
+    private MovementType movementType;
+   
+    
+    private Vector3 movementDirection3d;
+
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       PerformMovement();
+    }
+
+    void PerformMovement()
+    {
+        
+        if (movementType == MovementType.TransformBased)
+        {
+            gameObject.transform.position += movementDirection3d * _velocity;
+        }
+        else if (movementType == MovementType.PhysicsBased)
+        {
+            _rigidbody.AddForce(movementDirection3d, selectedForceMode);
+        }
+    }
+    
+    void OnMovement(InputValue inputValue)
+    {
+        Vector2 movementDirection = inputValue.Get<Vector2>();
+        movementDirection3d = new Vector3(movementDirection.x, 0, movementDirection.y);
+    }
+
+    void OnJump(InputValue inputValue)
+    {
+        _rigidbody.AddForce(Vector3.up * _jumpForce, selectedForceMode);
+        //Vector3.up is shorthand for new Vector3(0,0,1)
+    }
+
+}
